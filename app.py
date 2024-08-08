0ee3fbaac795d0cb5ff3933843904b9d2e6f946d
swapniljyt <swapniljytkd888@gmail.com>
2024-08-08 11:32:55 +0000

    commi

--- app.py
+++ app.py
@@ -2,20 +2,20 @@ import streamlit as st
 import numpy as np
 import pickle
 
-# Load the trained model
+
 model_filename = '/root/src/predictive-maintenance-streamlit-app/models/best_model.pkl'  # Update this path
 with open(model_filename, 'rb') as file:
     model = pickle.load(file)
 
-# Load the PCA transformation
+
 pca_filename = '/root/src/predictive-maintenance-streamlit-app/models/pca_transformer.pkl'  # Update this path
 with open(pca_filename, 'rb') as file:
     pca = pickle.load(file)
 
-# Define the Streamlit app
+
 st.title('Predictive Maintenance Model')
 
-# Input features
+
 st.header('Enter the input features:')
 air_temp = st.number_input('Air temperature [K]', value=298.1)
 process_temp = st.number_input('Process temperature [K]', value=308.6)
@@ -23,24 +23,17 @@ rot_speed = st.number_input('Rotational speed [rpm]', value=1551)
 torque = st.number_input('Torque [Nm]', value=42.8)
 tool_wear = st.number_input('Tool wear [min]', value=0.0)
 
-# One-hot encoded categorical features
+
 twf = st.selectbox('TWF (Tool Wear Failure)', [0, 1])
 hdf = st.selectbox('HDF (Heat Dissipation Failure)', [0, 1])
 pwf = st.selectbox('PWF (Power Failure)', [0, 1])
 osf = st.selectbox('OSF (Overstrain Failure)', [0, 1])
 
-# Create a button to make the prediction
+
 if st.button('Predict'):
-    # Prepare the input data
+
     input_data = np.array([[air_temp, process_temp, rot_speed, torque, tool_wear, twf, hdf, pwf, osf]])
-    
-    # Apply PCA transformation
-    input_data_pca = pca.transform(input_data)  # Use the loaded PCA object
-
-    # Make the prediction
+    input_data_pca = pca.transform(input_data)
     prediction = model.predict(input_data_pca)
-
-    # Display the prediction
     st.write(f'The predicted output is: {prediction[0]}')
 
-# To run the Streamlit app, save this script as `app.py` and run `streamlit run app.py` in your terminal
